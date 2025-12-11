f8b1cd48ee92dac9a7683aa450ec7d78697fff4e
devbyaziz <azizhabibie2002@gmail.com>
2025-12-11 22:02:47 +0700

    add main landmark and aria labels for better experience

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -385,6 +385,7 @@ const LandingPage = () => {
         />
       </nav>
 
+      <main>
       {/* Section Hero */}
       <section id="home" className={styles.heroSection}>
         <div className={styles.heroContent}>
@@ -947,7 +948,7 @@ const LandingPage = () => {
           <div className={styles.contactLinks}>
             {/* GitHub */}
             <div className={styles.contactItem}>
-              <a href="https://github.com/devbyaziz" target="_blank" rel="noopener noreferrer" className={styles.contactIconLink}>
+              <a href="https://github.com/devbyaziz" target="_blank" rel="noopener noreferrer" className={styles.contactIconLink} aria-label="Visit my GitHub profile">
                 <svg className={styles.contactIconSvg} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path d="M12 0C5.37 0 0 5.37 0 12C0 17.31 3.435 21.795 8.205 23.385C8.805 23.49 9.03 23.13 9.03 22.815C9.03 22.53 9.015 21.585 9.015 20.58C6 21.135 5.22 19.845 4.98 19.17C4.845 18.825 4.26 17.76 3.75 17.475C3.33 17.25 2.73 16.695 3.735 16.68C4.68 16.665 5.355 17.55 5.58 17.91C6.66 19.725 8.385 19.215 9.075 18.9C9.18 18.12 9.495 17.595 9.84 17.295C7.17 16.995 4.38 15.96 4.38 11.37C4.38 10.065 4.845 8.985 5.61 8.145C5.49 7.845 5.07 6.615 5.73 4.965C5.73 4.965 6.735 4.65 9.03 6.195C9.99 5.925 11.01 5.79 12.03 5.79C13.05 5.79 14.07 5.925 15.03 6.195C17.325 4.635 18.33 4.965 18.33 4.965C18.99 6.615 18.57 7.845 18.45 8.145C19.215 8.985 19.68 10.05 19.68 11.37C19.68 15.975 16.875 16.995 14.205 17.295C14.64 17.67 15.015 18.39 15.015 19.515C15.015 21.12 15 22.41 15 22.815C15 23.13 15.225 23.505 15.825 23.385C20.565 21.795 24 17.295 24 12C24 5.37 18.63 0 12 0Z"/>
                 </svg>
@@ -957,7 +958,7 @@ const LandingPage = () => {
 
             {/* LinkedIn */}
             <div className={styles.contactItem}>
-              <a href="https://linkedin.com/in/aziz-al-habibie-simatupang" target="_blank" rel="noopener noreferrer" className={styles.contactIconLink}>
+              <a href="https://linkedin.com/in/aziz-al-habibie-simatupang" target="_blank" rel="noopener noreferrer" className={styles.contactIconLink} aria-label="Visit my LinkedIn profile">
                 <svg className={styles.contactIconSvg} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
                 </svg>
@@ -967,7 +968,7 @@ const LandingPage = () => {
 
             {/* Gmail */}
             <div className={styles.contactItem}>
-              <a href="mailto:azizhabibie2002@gmail.com" className={styles.contactIconLink}>
+              <a href="mailto:azizhabibie2002@gmail.com" className={styles.contactIconLink} aria-label="Send me an email">
                 <svg className={styles.contactIconSvg} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path d="M24 5.457V18.543C24 19.893 22.893 21 21.543 21H2.457C1.107 21 0 19.893 0 18.543V5.457C0 4.107 1.107 3 2.457 3H21.543C22.893 3 24 4.107 24 5.457ZM21.543 5.457L12 11.793L2.457 5.457V6.771L12 13.107L21.543 6.771V5.457Z"/>
                 </svg>
@@ -992,6 +993,7 @@ const LandingPage = () => {
           </p>
         </div>
       </footer>
+      </main>
     </div>
   );
 };
